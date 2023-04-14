efd31b75e8e5749955ff32625ee55dfe4f2e84d7
jokerav <jokeravdeveloper@gmail.com>
2023-04-14 19:53:27 +0300

    boy

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -1,5 +1,7 @@
 import './user.scss'
 import {ReactComponent as Logo} from "../../img/logo.svg";
+import boy from '../../img/Boy.png'
+import boy2x from '../../img/Boy@2x.png'
 
 const User = ()=>{
     return(
@@ -7,6 +9,10 @@ const User = ()=>{
             <Logo className='logo'/>
             <div className='picture'></div>
             <div className='line'></div>
+            <picture>
+                <source srcSet={`${boy2x} 2x, ${boy} 1x`}/>
+                <img src='#' alt='Фото' className='photo'/>
+            </picture>
         </div>
     )
 }
