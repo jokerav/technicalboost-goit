ba6f6462398a7a74832a3cd090ff9b0cc283beff
jokerav <jokeravdeveloper@gmail.com>
2023-04-14 19:18:53 +0300

    card

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -1,6 +1,13 @@
+import './user.scss'
+import {ReactComponent as Logo} from "../../img/logo.svg";
+
 const User = ()=>{
     return(
-        <div>This is new user</div>
+        <div className='card'>
+            <Logo className='logo'/>
+            <div className='picture'></div>
+            <div className='line'></div>
+        </div>
     )
 }
 export default User
