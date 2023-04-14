f56881f3a6d4070393b72ea83040bfd2cf1164f7
jokerav <jokeravdeveloper@gmail.com>
2023-04-14 21:25:09 +0300

    card

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -9,10 +9,13 @@ const User = ()=>{
             <Logo className='logo'/>
             <div className='picture'></div>
             <div className='line'></div>
-            <picture>
+            <picture className='photo'>
                 <source srcSet={`${boy2x} 2x, ${boy} 1x`}/>
-                <img src='#' alt='Фото' className='photo'/>
+                <img src='#' alt='Фото' />
             </picture>
+            <div className='tweets'>777 tweets</div>
+            <div className='followers'>100500 Followers</div>
+            <button className='btnFollow'>follow</button>
         </div>
     )
 }
