912d93d684cbdd1bb01fbb2aaa68793e96e7229c
jokerav <jokeravdeveloper@gmail.com>
2023-04-15 10:29:11 +0300

    grid

--- src/components/usersList/usersList.js
+++ src/components/usersList/usersList.js
@@ -1,11 +1,11 @@
 import {useGetAllUsersQuery} from "../../store/usersAPI";
 import User from '../user/user'
-
+import "./userList.scss"
 const UsersList=()=>{
     const {data=[]} = useGetAllUsersQuery()
     console.log(data)
     return (
-        <ul>
+        <ul className='userList'>
             {data.length > 0 && data.map(user=>
             <User
                 key={user.id}
