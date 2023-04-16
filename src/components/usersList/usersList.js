fb7731d7283bef54e5a73e11e4886b6686dbd451
jokerav <jokeravdeveloper@gmail.com>
2023-04-16 20:55:15 +0300

    Update usersList.js

--- src/components/usersList/usersList.js
+++ src/components/usersList/usersList.js
@@ -1,18 +1,41 @@
 import {useGetAllUsersQuery} from "../../store/usersAPI";
 import User from '../user/user'
 import "./userList.scss"
+import {useDispatch, useSelector} from "react-redux";
+import {getPage} from "../../store/selectors";
+import {useEffect, useState} from "react";
+import {addPage, resetPage} from "../../store/dataSlice";
 
 const UsersList = () => {
+    const dispatch = useDispatch();
     const {data = []} = useGetAllUsersQuery();
+    let page = useSelector(getPage);
+    console.log(`${page} current page`);
+    let [visibleUsers, setVisibleUsers] = useState(data.slice(0, 8));
+    console.log(visibleUsers)
+    useEffect(() => {
+        setVisibleUsers(data.slice(0, page*8))
+    }, [page, data])
     return (
-        <ul className='userList'>
-            {data.length > 0 && data.map(user =>
-                <User
-                    key={user.id}
-                    user={user}
-                />
-            )}
-        </ul>
+        <div>
+            <ul className='userList'>
+                {visibleUsers.length > 0 && visibleUsers.map(user =>
+                    <User
+                        key={user.id}
+                        user={user}
+                    />
+                )}
+            </ul>
+            <button onClick={()=>{
+                dispatch(addPage());
+                // page = page+1;
+            }}>Load more
+            </button>
+            <button onClick={()=> {
+                dispatch(resetPage());
+                // page = 1;
+            }}>Reset page</button>
+        </div>
     )
 
 }
